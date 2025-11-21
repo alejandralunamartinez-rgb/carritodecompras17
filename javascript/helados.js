e8b0dad10d55b4f580352711539fca11bf879861
alejandralunamartinez-rgb <alejandra.lunamartinez@intjem.edu.bo>
2025-11-21 11:01:57 -0400

    Add files via upload

---
+++ javascript/helados.js
@@ -0,0 +1,56 @@
+const helados = [
+    {
+    id: 1,
+    nombre: "Gelatina Flor Azul",
+    img: "img/1.jpg",
+    precio: 35  
+    },
+    {
+    id: 2,
+    nombre: "Gelatina Girasol Rosa",
+    img: "img/2.jpg",
+    precio: 17
+    },
+    {
+    id: 3,
+    nombre: "Gelatina 3D con Leche",
+    img: "img/3.jpg",
+    precio: 40
+    },
+    {
+    id: 4,
+    nombre: "Torta 3D de Cumpleaños",
+    img: "img/4.jpg",
+    precio: 70
+    },
+    {
+    id: 5,
+    nombre: "Gelatina Flor Morada",
+    img: "img/5.jpg",
+    precio: 55
+    },
+    {
+    id: 6,
+    nombre: "Gelatina Flor Rosita",
+    img: "img/6.jpg",
+    precio: 25
+    },
+    {
+    id: 7,
+    nombre: "Tota de Gelatina 3D con Frutillas",
+    img: "img/7.jpg",
+    precio: 40
+    },
+    {
+    id: 8,
+    nombre: "Gelatinas 3D (por cada una)",
+    img: "img/8.jpg",
+    precio: 10
+    },
+    {
+    id: 9,
+    nombre: "Torta de Gelatina 3D de cuatro pisos",
+    img: "img/9.jpg",
+    precio: 100
+    }
+]
